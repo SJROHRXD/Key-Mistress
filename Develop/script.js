b608c5702d765519fb9faa8a87cc1d870ae57e57
SJRXD <rohr.olasz@gmail.com>
2023-07-19 15:53:58 -0700

    Fixing validation error for NaN, output for completed password, and considering a custom modal

--- Develop/script.js
+++ Develop/script.js
@@ -64,13 +64,14 @@ var possibleChars = [];
 function generatePassword() {
     const getPasswordLength = () => {
         const length = parseInt(prompt("Enter the length of the password (between 8 and 128):"));
-        if (length < 8 || length > 128) {
+        if (isNaN(length) || length < 8 || length > 128) {
           alert("Invalid password length. Please try again.");
           return getPasswordLength();
         }
         return length;
     }
     const length = getPasswordLength();
+    console.log(length);
 
     let possibleChars = "";
 
@@ -104,6 +105,7 @@ function generatePassword() {
               possibleChars += String.fromCharCode(i);
             }
           }
+        }
 
     if (possibleChars.length === 0) {
         alert("At least one character type must be selected.");
@@ -112,16 +114,15 @@ function generatePassword() {
 
         let generatedPassword = "";
 
-            for (let i = 0; i < passwordLength; i++) {
+            for (let i = 0; i < length; i++) {
             const randomIndex = Math.floor(Math.random() * possibleChars.length);
             generatedPassword += possibleChars[randomIndex];
         }
-
         // fix this cuz I need a shuffle algorithm
+    
+    // Display the password in the textarea
+    const passwordField = document.querySelector("#password");
+    passwordField.value = generatedPassword;
+  }
 
-    // You can now display the password and copy it to the clipboard.
-    console.log(generatedPassword);
-}
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
